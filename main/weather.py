c258a86179aefb0670330dd3cf6d4f068a2b6615
linhy <hjklin@gmail.com>
2016-04-02 00:27:22 +0800

    加angular.js

--- main/weather.py
+++ main/weather.py
@@ -5,7 +5,7 @@ from common.secret_const import baidu_apistore_api_key as api_key
 # 返回结果说明：http://apistore.baidu.com/apiworks/servicedetail/478.html
 import logging
 log = logging.getLogger(__name__)
-import time
+import datetime
 
 import baidu_tts
 from common.util import getJson,byteify
@@ -28,7 +28,9 @@ def today(drsg = True, trav = False):
     w = json['HeWeather data service 3.0'][0]
     now_tmp = w['now']['tmp']  # 当前温度
 
-    today = time.strftime('%Y-%m-%d')
+    today = datetime.date.today().strftime('%Y-%m-%d')
+
+
 
     fc = filter(lambda d:d['date']==today, w['daily_forecast'])
     if len(fc) == 1 :
@@ -66,8 +68,48 @@ def today(drsg = True, trav = False):
            (now_tmp, d_cond, n_cond, min_tmp, max_tmp, comf, drsg, trav)
 
 
+def day_of_week(days_after):
+    """一周内的天气，days_after：几天后; 0即今天。"""
+
+    if days_after == 0: return today()
+    if days_after < 0 or days_after > 6: raise ValueError(u"日期需在[0-6]间")
+
+    json = byteify(getJson(url, header=header))
+
+    if 'HeWeather data service 3.0' not in json or json['HeWeather data service 3.0'][0]['status'] != 'ok':
+        log.error("天气信息获取失败：%s" % json)
+        return "天气信息获取失败"
+
+    w = json['HeWeather data service 3.0'][0]
+
+    day = datetime.date.today() +datetime.timedelta(days=days_after)
+    day_str = day.strftime('%Y-%m-%d')
+
+    fc = filter(lambda d: d['date'] == day_str, w['daily_forecast'])
+    if len(fc) == 1:
+        day_forecast = fc[0]
+    else:
+        log.error("无法获取%s的天气：%s" % (day.strftime('%m/%d'),w['daily_forecast']))
+        return "无法获取%s的天气"%day.strftime('%m/%d')
+    d_cond = day_forecast['cond']['txt_d']  # 白天天气
+    n_cond = day_forecast['cond']['txt_n']  # 夜间天气
+
+    min_tmp = day_forecast['tmp']['min']
+    max_tmp = day_forecast['tmp']['max']
+
+
+    log.info("获取%s天气。更新日期：%s" % (day_forecast['date'], w['basic']['update']['loc']))
+    return "%s白天：%s，夜间：%s，%s到%s度。" % \
+           (day.strftime('%m/%d'), d_cond, n_cond, min_tmp, max_tmp)
+
+
+
 def tell_today(drsg = True, trav = False):
     w = today(drsg, trav)
+    baidu_tts.read_aloud(w)
+
+def tell_day_of_week(days_after):
+    w = day_of_week(days_after)
     baidu_tts.read_aloud(w)
 
 if __name__ == '__main__':
