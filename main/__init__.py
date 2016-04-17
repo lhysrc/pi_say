b58a83665930afa325910b7cd39745f627dd8c29
linhy <hjklin@gmail.com>
2016-04-17 16:05:54 +0800

    完善任务

---
+++ main/__init__.py
@@ -0,0 +1,7 @@
+# import os
+#
+# required_dirs = ['tmp','music_files','log']
+# for d in required_dirs:
+#     if not os.path.exists(d):
+#         os.mkdir(d)
+#
--- main/task.py
+++ main/task.py
@@ -7,7 +7,7 @@
 import baidu_tts
 import config
 import gz_bus
-import play_sound
+# import play_sound
 import weather
 import music
 
@@ -41,7 +41,7 @@ class Task(object):
     # t_hour = 0
     # t_min = 0
     # type = 3 # 1 工作日，2 假日
-    def __init__(self,name,hr,mn,func,args=(),type=1,pause_days=0):
+    def __init__(self,name,hr,mn,func,args=(),type=3,pause_days=0):
         self.name = name
         self.func = func
         self.args = args
@@ -68,29 +68,30 @@ class Task(object):
     #         self._reset_time = datetime.datetime.today() - datetime.timedelta(days=value)
 
 
-class PlayLocalMusicTask(Task):
-    def __init__(self, hr, mn, path, n, type=3 ,pause_days = 0):
-        self.path = path
-        self.n = n
-        super(PlayLocalMusicTask, self).__init__(u"播放本地音乐",
-                                                 hr, mn, music.play_songs, (path, n, True), type,pause_days)
+# class PlayLocalMusicTask(Task):
+#     def __init__(self, hr, mn, path, n, type=3 ,pause_days = 0):
+#         self.path = path
+#         self.n = n
+#         super(PlayLocalMusicTask, self).__init__(u"播放本地音乐",
+#                                                  hr, mn, music.play_songs, (path, n, True), type,pause_days)
+#
+#     def __repr__(self):
+#         return "%s(%s,%s,'%s',%s,%s,%s) " % (
+#             self.__class__.__name__, self.t_hour, self.t_min, self.path, self.n, self.type, self.pause_days)
+#
 
-    def __repr__(self):
-        return "%s(%s,%s,'%s',%s,%s,%s) " % (
-            self.__class__.__name__, self.t_hour, self.t_min, self.path, self.n, self.type, self.pause_days)
 
-
-
-class PlayNetMusicTask(Task):
-    def __init__(self, hr, mn,url, n,  type=3,pause_days = 0):
+class PlayMusicTask(Task):
+    def __init__(self, hr, mn,url, n, vol=None, type=3,pause_days = 0):
         self.url = url
         self.n = n
-        super(PlayNetMusicTask, self).__init__(u"播放网络音乐",
-                                               hr, mn, music.play_songs, (url, n, True), type, pause_days)
+        self.vol = vol
+        super(PlayMusicTask, self).__init__(u"播放音乐",
+                                            hr, mn, music.play_songs, (url, n, True, vol), type, pause_days)
 
     def __repr__(self):
-        return "%s(%s,%s,'%s',%s,%s,%s) " % (
-            self.__class__.__name__, self.t_hour, self.t_min, self.url, self.n, self.type,self.pause_days)
+        return "%s(%s,%s,'%s',%s,%s,%s,%s) " % (
+            self.__class__.__name__, self.t_hour, self.t_min, self.url, self.n, self.vol, self.type,self.pause_days)
 
 class TellBusTask(Task):
     def __init__(self, hr, mn,search_id, cnt=15, interval=60, type=1,pause_days = 0):
@@ -142,7 +143,7 @@ class ReadTextTask(Task):
 
 
 @tell_time.tell_time_first
-def load_weather(d=1,t=''):
+def load_weather(d=True,t=False):
     """
         d:穿衣建议
         t:出行建议
@@ -164,13 +165,15 @@ def auto_check_in():
         util.sendEmail('hyiit@qq.com','虾米签到失败',ret)
 
 
-tasks={}
+
 def load_all_tasks():
+    tasks = {}
     names = config.config.options('Task')
     for name in names:
         tasks[name] = eval(config.config.get('Task',name))
+    return tasks
 
-def save_all_tasks():
+def save_all_tasks(tasks):
     for name in tasks.keys():
         config.set('Task',name,tasks[name])
     config.save()
@@ -194,25 +197,35 @@ def save_task(name,task):
 
 
 if __name__ == '__main__':
+    #
+    # rtt = ReadTextTask(8,10,"你是呆逼吗？")
+    # print rtt
 
-    rtt = ReadTextTask(8,10,"你是呆逼吗？")
-    print rtt
-
-    tasks = {
-        'xm': Task("虾米签到", 9, 0, auto_check_in, (), 3),
-        'weather1': TellWeatherTask(8, 55),
-        'wy': PlayNetMusicTask(14, 0, "http://music.163.com/#/playlist?id=326005981", 5)
-    }
-    load_all_tasks()
+    # tasks = {
+    #     'db': ReadTextTask(14,59,"你是呆逼吗？"),
+    #     'xm': Task("虾米签到", 9, 0, auto_check_in),
+    #     'weather1': TellWeatherTask(14, 56),
+    #     'wy': PlayMusicTask(15, 58, "http://music.163.com/#/playlist?id=326005981", 5,pause_days=2)
+    # }
+    tasks = load_all_tasks()
+    # save_all_tasks(tasks)
     # print task.tasks
     from threading import Timer
     import time
+
+    for tktk in tasks:
+        if isinstance(tasks[tktk],PlayMusicTask):
+            print tasks[tktk]
 
     t = time.localtime()
     for name in tasks:
         tk = tasks[name]
         if t.tm_hour == tk.t_hour and tk.t_min >= t.tm_min:
-            interval = (tk.t_min - t.tm_min) * 60 - t.tm_sec
-            interval = max(0, interval)
-            log.warn(u"将在%s秒后执行任务：%s - %s。" % (interval, name.decode('utf-8'), tk.name))
-            Timer(interval, tk.func, tk.args).start()
+            if tk.pause_days == 0:
+                interval = (tk.t_min - t.tm_min) * 60 - t.tm_sec
+                interval = max(0, interval)
+                log.warn(u"将在%s秒后执行任务：%s - %s。" % (interval, name.decode('utf-8'), tk.name))
+                Timer(interval, tk.func, tk.args).start()
+            elif tk.pause_days > 0:
+                tk.pause_days -= 1
+                save_task(name,tk)
